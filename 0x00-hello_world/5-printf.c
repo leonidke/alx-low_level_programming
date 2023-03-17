36798508dc55251a66d7bd0e58dbd40f9d430e44
leonidke <107030484+leonidke@users.noreply.github.com>
2023-03-17 07:11:52 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
@@ -9,4 +8,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
