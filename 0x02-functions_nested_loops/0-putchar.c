e559c1cfd9f86228130354203d72233d5374a0dd
leonidke <leonisawe@gmail.com>
2023-03-22 08:42:50 -0700

    Program to print _putcher

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints _putchar, followed by a new line.
- * Return: 0
-**/
-
+ * main - Entry point.
+ * Description: write a program that prints "_putchar" followed by a new line.
+ * Return: Always 0 (Success.)
+ */
 int main(void)
 {
-	unsigned int i;
-	char c[] = "_putchar";
-
-	for (i = 0; i <= 7; i++)
-	{
-		_putchar(c[i]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
