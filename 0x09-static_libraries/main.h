d52f82da0d99bf6e7b35f7c0a3630e72158aefdb
leonidke <leonisawe@gmail.com>
2023-04-10 17:08:38 +0300

    Function Prototypes

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 /* Function prototypes */
 int _putchar(char c);
 int _islower(int c);
@@ -19,3 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
