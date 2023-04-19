8bf93e8a31566b28f6db098bfef4a38f975f7664
leonidke <leonisawe@gmail.com>
2023-04-19 22:16:36 +0300

    Function pointer header file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,8 +10,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned i;
+	unsigned int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (size && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define _FUNCPOINTER_H_
 
 #include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
