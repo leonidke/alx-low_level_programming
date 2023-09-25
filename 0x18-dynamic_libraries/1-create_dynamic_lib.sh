43578eabab58a26a1b7170fe1d7a9884e99ecf8f
leonidke <leonisawe@gmail.com>
2023-09-25 15:16:17 +0300

    script to create dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c isdigit.c memset.c strchr.c strncat.c strstr.c abs.c islower.c _putchar.c strcmp.c strncpy.c atoi.c isupper.c puts.c strcpy.c strpbrk.c isalpha.c memcpy.c strcat.c strlen.c strspn.c
-gcc -shared -o *.o -o liball.so
-rm *.o
+#!/bin/bash
+gcc -fPIC -c *.c
+gcc -shared -o liball.so *.o
+
